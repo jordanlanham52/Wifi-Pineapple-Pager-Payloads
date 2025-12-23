36aaa628cee0246cdff5ba2abd0a00ea7617e1aa
Jordan Lanham <125485915+jordanlanham52@users.noreply.github.com>
2025-12-23 11:39:17 -0700

    Fix ringtone name in payload.sh

--- payloads/user/testing/TEST_RINGTONES/payload.sh
+++ payloads/user/testing/TEST_RINGTONES/payload.sh
@@ -5,4 +5,4 @@
 # Date: 2025-12-23
 # Version: 1.0
 
-RINGTONE RickRoll_DesertYouFixed:d=8,o=5,b=112:a,b,d6,b,f#6,f#6,e6,4p,a,b,d6,b,e6,e6,d6,c#6,b,4p,a,b,d6,b,d6,e6,c#6,4a,8a,8e6,4d6,4p,a,b,d6,b,f#6,f#6,e6,4p,a,b,d6,b,a6,c#6,d6,c#6,b,4p,a,b,d6,b,d6,e6,c#6,4a,8a,8e6,4d6
+RINGTONE RickRoll:d=8,o=5,b=112:a,b,d6,b,f#6,f#6,e6,4p,a,b,d6,b,e6,e6,d6,c#6,b,4p,a,b,d6,b,d6,e6,c#6,4a,8a,8e6,4d6,4p,a,b,d6,b,f#6,f#6,e6,4p,a,b,d6,b,a6,c#6,d6,c#6,b,4p,a,b,d6,b,d6,e6,c#6,4a,8a,8e6,4d6
